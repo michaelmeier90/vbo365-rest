e43c8457a3cd5a4a0477f786f6deef978310eaae
michaelmeier90 <60002793+michaelmeier90@users.noreply.github.com>
2021-04-08 11:27:45 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$host = '127.0.0.1'; /* Veeam Backup for Microsoft Office 365 server (hostname or IP) */
+$host = '10.50.30.150'; /* Veeam Backup for Microsoft Office 365 server (hostname or IP) */
 $port = '4443'; /* RESTful API service port (default: 4443) */
-$title = 'Veeam Backup for Microsoft Office 365'; /* Custom title for the portal to be displayed in the browser title */
+$title = 'e-novinfo - VBO Portal'; /* Custom title for the portal to be displayed in the browser title */
 $version = 'v5'; /* RESTful API version (default: v5) */
 $administrator = 'yes'; /* Allow Windows administrator accounts to be used as a login (yes or no) */
 $limit = '250'; /* Amount of items to load for exploring restore points (default: 250) - a higher number may impact loading performance */
